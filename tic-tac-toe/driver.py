6c6a9dfc7da27ae9f9c9750a584b8e67e22b99ef
Leon Nguyen <leonn@vt.edu>
2021-09-28 22:36:10 -0400

    Set up a 3 by 3 grid that will be used for the main
    gameplay

--- tic-tac-toe/driver.py
+++ tic-tac-toe/driver.py
@@ -6,11 +6,41 @@ Created on Sep 28, 2021
 
 from tkinter import *
 
-
 window = Tk()
 
 window.title("Tic Tac Toe GUI")
 
-window.geometry("600x600")
+x = 625
+
+y = 625
+
+window.geometry("" + str(x) + "x" + str(y))
+
+buttons = []
+
+pixelVirtual = PhotoImage(width=1, height=1)
+
+def clicked(x):
+    print(str(x))
+    #Button.configure(height = 100, width = 100)
+
+
+for x in range(9):
+    
+    c = int(x % 3)
+    
+    r = int(x / 3)
+    
+    tempButton = Button(window,
+                          text="" + str(x),
+                          image=pixelVirtual,
+                          width=200,
+                          height=200,
+                          compound="c",
+                          command=clicked(x) )
+    
+    buttons.append(tempButton)
+    
+    buttons[x].grid(column=c , row=r)
 
 window.mainloop()
